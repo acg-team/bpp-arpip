26ef9c20bd98c76a97f12127b0c245b746bf2f92
Gholamhossein Jowkar <ghjowk@yahoo.co.uk>
2021-10-05 17:35:20 +0200

    Added: PIPAncestralStateReconstruction.cpp/h: This is the implementation of Ancestral Sequence reconstruction under PIP.
    *PIPAncestralStateReconstruction.cpp/h
     -  Ancestral Sequence reconstruction under PIP using Dynamic programming similar to Pupko's FastML.

---
+++ src/PIP/AncestralStateReconstruction/PIPAncestralStateReconstruction.cpp
@@ -0,0 +1,347 @@
+/*******************************************************************************
+ * Licensed Materials - Property of Gholamhossein Jowkar
+ *
+ *
+ * Copyright (C) 2019-2023 by Gholamhossein Jowkar
+ *
+ * This work was supported by the Swiss National Science Foundation (SNF) grants $31003A\_176316$ to Dr. M. Anisimova.
+ * The funding body did not play any role in the design of the study and collection, analysis, and interpretation of
+ * data and in writing the code.
+ *******************************************************************************
+ *
+ * This file is part of ARPIP project
+ *
+ * ARPIP: Ancestral Sequence Reconstruction with insertions and deletions under the Poisson Indel Process
+ * ARPIP is a joint maximum likelihood approach for phylogenetic ancestral sequence reconstruction, capable of modeling
+ * indels both biological and mathematically.
+ *
+ *
+ * This software is based and extends the following libraries:
+ *
+ * - the Bio++ libraries
+ *   developed by the Bio++ Development Team <http://biopp.univ-montp2.fr>
+ *
+ *
+ * ARPIP is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ARPIP is a free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ARPIP. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+/**
+ * @file PIPAncestralStateReconstruction.cpp
+ * @author Gholamhossein Jowkar
+ * @date 11 08 2020
+ * @version 1.0.0
+ * @maintainer Gholamhossein Jowkar
+ * @email jowk@zhaw.ch
+ * @status Development
+ *
+ * @brief
+ * @details
+ * @pre
+ * @bug
+ * @warning
+ *
+ * @see For more information visit:
+ */
+// From ARPIP:
+#include "PIPAncestralStateReconstruction.h"
+
+// Form bpp:
+#include<Bpp/Numeric/VectorTools.h>
+#include<Bpp/Numeric/Random/RandomTools.h>
+
+using namespace bpp;
+
+/************************************************* Constructor ********************************************************/
+PIPAncestralStateReconstruction::PIPAncestralStateReconstruction(const PIPDRTreeLikelihood *lik, const PIPMLIndelPoints *mlIndelPoints):
+        likelihood_(lik),
+        mlIndelPoints_(mlIndelPoints),
+        tree_(lik->getTree()),
+        alphabet_(lik->getAlphabet()),
+        nbInnerNodes_(lik->getTree().getInnerNodesId().size()),
+        nbSites_(lik->getLikelihoodData()->getNumberOfSites()),
+        nbDistinctSites_(lik->getLikelihoodData()->getNumberOfDistinctSites()),
+        nbStates_(lik->getLikelihoodData()->getNumberOfStates()),
+        rootPatternLinks_(lik->getLikelihoodData()->getRootArrayPositions())
+{
+    DLOG(INFO) << "PIPAncestralStateReconstruction::PIPAncestralStateReconstruction. is constructed successfully.";
+}
+
+/********************************************* Copy constructor *******************************************************/
+PIPAncestralStateReconstruction::PIPAncestralStateReconstruction(const PIPAncestralStateReconstruction &arpip):
+        likelihood_(arpip.likelihood_),
+        mlIndelPoints_(arpip.mlIndelPoints_),
+        tree_(arpip.tree_),
+        alphabet_(arpip.alphabet_),
+        nbInnerNodes_(arpip.nbInnerNodes_),
+        nbSites_(arpip.nbSites_),
+        nbDistinctSites_(arpip.nbDistinctSites_),
+        nbStates_(arpip.nbStates_),
+        rootPatternLinks_(arpip.rootPatternLinks_){}
+
+/********************************************* Operator overloading *******************************************************/
+
+PIPAncestralStateReconstruction &PIPAncestralStateReconstruction::operator=(const PIPAncestralStateReconstruction &arpip){
+    likelihood_         = arpip.likelihood_;
+    mlIndelPoints_      = arpip.mlIndelPoints_;
+    tree_               = arpip.tree_;
+    alphabet_           = arpip.alphabet_;
+    nbInnerNodes_       = arpip.nbInnerNodes_;
+    nbSites_            = arpip.nbSites_;
+    nbDistinctSites_    = arpip.nbDistinctSites_;
+    nbStates_           = arpip.nbStates_;
+    rootPatternLinks_   = arpip.rootPatternLinks_;
+
+    return *this;
+}
+/****************************************** PIP based functions **********************************************/
+/**********************************************************************************************************************/
+
+void PIPAncestralStateReconstruction::getAncestralStatesForSite(const Node *node,
+                                                                std::map<int, std::vector<int> > &ancestors,
+                                                                VVdouble &lArray, VVdouble &cArray,
+                                                                const size_t siteNumber, const int newRootId) const {
+
+    int nodeId = node->getId();
+    if (nodeId == newRootId) {
+        ancestors[nodeId][siteNumber] = VectorTools::whichMax(lArray[nodeId]);
+    } else {
+        const Node *father = node->getFather();
+        int winnerIndex = ancestors[father->getId()][siteNumber];
+        ancestors[nodeId][siteNumber] = cArray[nodeId][winnerIndex];
+    }
+
+}
+/**********************************************************************************************************************/
+
+std::map<int, std::vector<int>> PIPAncestralStateReconstruction::getAllAncestralStatesWGap() const {
+
+    std::map<int, std::vector<int>> ancestors;
+
+    // Pupko's Arrays:
+    VVVdouble likelihoodArray;
+    VVVdouble cLikelihoodArray;
+
+    // Allocating the mentioned arrays:
+    likelihoodArray.resize(nbDistinctSites_);
+    cLikelihoodArray.resize(nbDistinctSites_);
+
+    // Clone the data into a AlignedSequenceContainer for more efficiency:
+    AlignedSequenceContainer *data = new AlignedSequenceContainer(*likelihood_->getLikelihoodData()->getShrunkData());
+    DVLOG(2) << "[AR] Cloning the data with "<< data->getNumberOfSites()<<" sites.";
+
+    // Indel points extracted using MLIndelPoints algorithm:
+    std::map<size_t, std::vector<std::string>> insertionPoints = mlIndelPoints_->getInsertionPoints();
+    std::map<size_t, std::vector<std::string>> deletionPoints = mlIndelPoints_->getDeletionPoints();
+    DVLOG(2) << "[AR] The insertion and deletion points copied from MLIndelPoints object containing " <<
+             insertionPoints.size() << " Insertion points and " << deletionPoints.size() << " deletion points.";
+
+    // List of internal nodes:
+    std::vector<int> ancestorNodeListNb = tree_.getInnerNodesId();
+
+    for (size_t siteNb = 0; siteNb < nbDistinctSites_; siteNb++) {
+        // Copying the existing tree:
+        TreeTemplate<Node> *PIPSubTree = tree_.clone();
+        if ((insertionPoints[siteNb].at(0).empty()))
+            throw Exception(
+                    "PIPAncestralStateReconstruction::getAllAncestralStatesWGap. Error, NO Insertion point for this site!");
+        // Extracting the subtree based on insertion point:
+        const Node *newRoot = PIPSubTree->getNode(std::stoi(insertionPoints[siteNb][0]), 0);
+        DVLOG(2) << "[AR] The new root for site number " << siteNb << " is node number: " << newRoot->getId();
+
+        // case newroot != treeRoot
+        if(PIPSubTree->getRootId() != newRoot->getId()){
+            PIPSubTree = PIPSubTree->cloneSubtree(newRoot->getId());
+            DVLOG(2)<< "[AR] The new root is different from the tree root.";
+        }
+
+        // Initialize the ancestor array with gap for both scenario that there are deletions and an insertion different than the root.
+        for(auto innerNodeNb:ancestorNodeListNb){
+            ancestors[innerNodeNb].resize(nbDistinctSites_);
+            ancestors[innerNodeNb][siteNb]= -1;//char for gap state
+            DVLOG(2) << "[AR] The ancestor value for internal node nb " << innerNodeNb << " and siteNb " << siteNb
+                     << " is initialized by:" << ancestors[innerNodeNb][siteNb];
+        }
+        std::vector<int> PIPDeletionPoints;
+        if (!newRoot->isLeaf()) {
+            for(auto &str:deletionPoints[siteNb]) {
+                PIPDeletionPoints.push_back(std::stoi(str));
+                if(PIPSubTree->isLeaf(std::stoi(str))){
+                    Node *fNode = PIPSubTree->getNode(std::stoi(str))->getFather();
+                    Node *cNode = PIPSubTree->getNode(std::stoi(str));
+                    fNode->removeSon(cNode);
+                } else {
+
+                    Node *fNode = PIPSubTree->getNode(std::stoi(str))->getFather();
+                    Node *cNode = PIPSubTree->getNode(std::stoi(str));
+                    fNode->removeSon(cNode);
+                }
+            }
+
+            recursiveJointAncestralStates(newRoot, PIPSubTree, ancestors, likelihoodArray, cLikelihoodArray,
+                                          *data, siteNb, newRoot->getId(), 0);
+        }
+        delete newRoot;
+    }
+    delete data;
+    return ancestors;
+}
+/**********************************************************************************************************************/
+
+void PIPAncestralStateReconstruction::recursiveJointAncestralStates(const Node *node, const TreeTemplate<Node> *tree,
+                                                                    std::map<int, std::vector<int> > &ancestors,
+                                                                    VVVdouble &likelihoodArray, VVVdouble &cLikelihoodArray,
+                                                                    AlignedSequenceContainer &data,
+                                                                    const size_t siteNumber,
+                                                                    const int newRootId, bool isVisited) const {
+
+    VVdouble *larray = &likelihoodArray[siteNumber];
+    VVdouble *carray = &cLikelihoodArray[siteNumber];
+
+    bool *visited = &isVisited;
+
+    if (!(*visited)) {
+        *visited = true;
+        recursiveComputeLikelihoodAtSite(node, tree, *larray,*carray, siteNumber, newRootId);
+
+    }
+
+    if (!node->isLeaf()) {
+        getAncestralStatesForSite(node, ancestors, *larray, *carray, siteNumber, newRootId);
+        for (size_t i = 0; i < node->getNumberOfSons(); i++) {
+            recursiveJointAncestralStates(node->getSon(i), tree, ancestors, likelihoodArray, cLikelihoodArray, data, siteNumber,
+                                          newRootId, isVisited);
+        }
+    }
+
+}
+/**********************************************************************************************************************/
+
+void PIPAncestralStateReconstruction::recursiveComputeLikelihoodAtSite(const Node *node,
+                                                                       const TreeTemplate<Node> *tree,
+                                                                       VVdouble &likelihoodArray,
+                                                                       VVdouble &characterArray,
+                                                                       const size_t siteNumber,
+                                                                       const int newRootId) const {
+
+    for (size_t i = 0; i < node->getNumberOfSons(); i++) {
+        recursiveComputeLikelihoodAtSite(node->getSon(i), tree, likelihoodArray, characterArray, siteNumber, newRootId);
+    }
+    // Compute Pupko's likelihood using interface defined:
+    likelihood_->computeLikelihoodAtSite(node, tree, likelihoodArray, characterArray,  siteNumber, newRootId);// From PIPDRHomogeneousTreeLikelihood
+
+
+}
+
+
+/**********************************************************************************************************************/
+// Joint version //todo: Marginal should be implemented.
+Sequence *PIPAncestralStateReconstruction::getAncestralSequenceForNode(int nodeId) const {
+
+    // Check to see nodeId is a member of internal nodes:
+    std::vector<int> InternalNodes = tree_.getInnerNodesId();
+    if (!(std::count(InternalNodes.begin(), InternalNodes.end(), nodeId)))
+    {
+        throw Exception(
+                "PIPAncestralStateReconstruction::getAncestralSequenceForNode(). nodeId is not member of internal nodes. (pick another node)");
+    }
+
+    // Do the joint reconstruction:
+    std::map<int, std::vector<int> > allNodes = getAllAncestralStatesWGap();
+    //Allocating the new array for sequence:
+    std::vector<int> allStates(nbSites_);
+
+    std::string name = tree_.hasNodeName(nodeId) ? tree_.getNodeName(nodeId) : ("" + TextTools::toString(nodeId));
+
+
+    DVLOG(2) << "[AR] Ancestral Sequence for node (" << nodeId << ") @node ";
+
+    // Mapping the shrunk data to the normal one:
+    for (int i = 0; i < nbSites_; ++i) {
+        // Extracting the position of the site in the shrunkData
+        allStates.at(i) = allNodes[nodeId][rootPatternLinks_[i]];
+        DVLOG(2) << "[AR] " << "COL " << i << " is: " << (mlIndelPoints_->getShrunkData()->getSite(i)).toString()
+                 << std::endl;
+        DVLOG(2) << "[AR] Ancestral Sequence reconstruction for Site (" << i << ") in Shrunk data ("
+                 << rootPatternLinks_[i] << ")" << " is: " << allStates.at(i);
+    }
+    return new BasicSequence(name, allStates, alphabet_);
+}
+
+/**********************************************************************************************************************/
+//// Joint version //todo: Marginal should be implemented.
+//std::vector<size_t> PIPAncestralStateReconstruction::getAncestralStatesForNode(int nodeId) const{
+//
+//    // Check to see nodeId is a member of internal nodes:
+//    std::vector<int> InternalNodes = tree_.getInnerNodesId();
+//    if (!(std::count(InternalNodes.begin(), InternalNodes.end(), nodeId)))
+//    {
+//        throw Exception(
+//                "PIPAncestralStateReconstruction::getAncestralStatesForNode(). nodeId is not member of internal nodes. (pick another node)");
+//    }
+//    // Do the joint reconstruction:
+//    std::map<int, std::vector<int> > allNodes = getAllAncestralStatesWGap);
+//
+//    return allNodes[nodeId];
+//}
+
+
+
+
+/**********************************************************************************************************************/
+
+AlignedSequenceContainer *PIPAncestralStateReconstruction::getAncestralSequences() const {
+
+    // Do the joint reconstruction:
+    std::map<int, std::vector<int>> allNodesAllStates = getAllAncestralStatesWGap();
+    DLOG(INFO) << "[Ancestral Sequence Reconstruction] Joint Ancestral Sequence Reconstruction is done successfully.";
+
+    // Allocate the new array for Sequences:
+    AlignedSequenceContainer *asc = new AlignedSequenceContainer(alphabet_);
+
+    // List of internal nodes:
+    std::vector<int> internalNodesIndex = tree_.getInnerNodesId();
+
+    for (size_t nodeIId = 0; nodeIId < nbInnerNodes_; nodeIId++) {
+
+        size_t currentNodeId = internalNodesIndex[nodeIId];
+
+        DVLOG(2) << "[Ancestral Sequence Reconstruction] Ancestral Sequence for node (" << nodeIId << ") @node ";
+        std::string name = tree_.hasNodeName(currentNodeId) ? tree_.getNodeName(currentNodeId) : ("" +
+                                                                                      TextTools::toString(currentNodeId));
+
+        std::vector<int> allStates;
+        allStates.empty();
+        allStates.resize(nbSites_);
+        // Mapping the shrunk data to the normal one:
+        for (size_t i = 0; i < nbSites_; i++) {
+
+            DVLOG(2) << "[Ancestral Sequence Reconstruction] " << "COL " << i << " is: "
+                     << (mlIndelPoints_->getShrunkData()->getSite(i)).toString() << std::endl;
+
+            size_t rootIndex = rootPatternLinks_[i];
+
+            // Extracting the position of the site in the shrunkData
+            allStates[i] = allNodesAllStates[currentNodeId][rootIndex];
+
+            DVLOG(2) << "[Ancestral Sequence Reconstruction] Ancestral Sequence reconstruction for Site (" << i << ") in Shrunk data ("
+                     << rootIndex << ")" << " is: " << allStates.at(i);
+        }
+
+        BasicSequence *seq = new BasicSequence(name, allStates, alphabet_);
+
+        asc->addSequence(*seq);
+        delete seq;
+    }
+    return asc;
+}
+/**********************************************************************************************************************/
---
+++ src/PIP/AncestralStateReconstruction/PIPAncestralStateReconstruction.h
@@ -0,0 +1,238 @@
+/*******************************************************************************
+ * Licensed Materials - Property of Gholamhossein Jowkar
+ *
+ *
+ * Copyright (C) 2019-2023 by Gholamhossein Jowkar
+ *
+ * This work was supported by the Swiss National Science Foundation (SNF) grants $31003A\_176316$ to Dr. M. Anisimova.
+ * The funding body did not play any role in the design of the study and collection, analysis, and interpretation of
+ * data and in writing the code.
+ *******************************************************************************
+ *
+ * This file is part of ARPIP project
+ *
+ * ARPIP: Ancestral Sequence Reconstruction with insertions and deletions under the Poisson Indel Process
+ * ARPIP is a joint maximum likelihood approach for phylogenetic ancestral sequence reconstruction, capable of modeling
+ * indels both biological and mathematically.
+ *
+ *
+ * This software is based and extends the following libraries:
+ *
+ * - the Bio++ libraries
+ *   developed by the Bio++ Development Team <http://biopp.univ-montp2.fr>
+ *
+ *
+ * ARPIP is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ARPIP is a free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ARPIP. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+/**
+ * @file PIPAncestralStateReconstruction.h
+ * @author Gholamhossein Jowkar
+ * @date 11 08 2020
+ * @version 1.0.0
+ * @maintainer Gholamhossein Jowkar
+ * @email jowk@zhaw.ch
+ * @status Development
+ *
+ * @brief
+ * @details
+ * @pre
+ * @bug
+ * @warning
+ *
+ * @see For more information visit:
+ */
+#ifndef ARPIP_PIPANCESTRALSTATERECONSTRUCTION_H
+#define ARPIP_PIPANCESTRALSTATERECONSTRUCTION_H
+
+// From Phyl:
+#include <Bpp/Phyl/AncestralStateReconstruction.h>
+
+// From ARPIP:
+#include "../Likelihood/PIPDRTreeLikelihood.h"
+#include "../Likelihood/PIPMLIndelPoints.h"
+
+// From Seqlib:
+#include <Bpp/Seq/Alphabet/Alphabet.h>
+#include <Bpp/Seq/Container/AlignedSequenceContainer.h>
+#include <Bpp/Seq/Sequence.h>
+
+// From STL:
+#include<vector>
+
+namespace bpp {
+    class PIPAncestralStateReconstruction :
+            public virtual AncestralStateReconstruction {
+    private:
+        const PIPDRTreeLikelihood *likelihood_;
+        const PIPMLIndelPoints *mlIndelPoints_;
+        TreeTemplate<Node> tree_;
+        const Alphabet *alphabet_;
+        size_t nbInnerNodes_;
+        size_t nbSites_;
+        size_t nbDistinctSites_;
+        size_t nbStates_;
+        std::vector<size_t> rootPatternLinks_;
+//        std::vector<double> r_;
+//        std::vector<double> l_;
+
+    public:
+        /**
+         * @brief Constructor
+         */
+        PIPAncestralStateReconstruction(const PIPDRTreeLikelihood *lik, const PIPMLIndelPoints *mlIndelPoints);
+
+        /**
+         * @brief Copy Constructor
+         */
+        PIPAncestralStateReconstruction(const PIPAncestralStateReconstruction &arpip);
+
+        /**
+         * @brief Operator Overloading
+         */
+        PIPAncestralStateReconstruction &operator=(const PIPAncestralStateReconstruction &arpip);
+
+        /**
+         * @brief Destructor
+         */
+         virtual ~PIPAncestralStateReconstruction(){}
+
+         PIPAncestralStateReconstruction *clone()const { return new PIPAncestralStateReconstruction(*this); }
+
+    public:
+
+        /****************************************** PIP based functions **********************************************/
+
+
+        /**
+         * @brief Get ancestral states for a given node at specific site using Pupko's method.
+         *
+         * The size of the vector is the number of distinct sites in the container
+         * associated to the likelihood object.
+         * This method is mainly for efficient internal use in other classes.
+         * Consider using the recursiveGetAncestralStatesForAllNodes() method for a more general output.
+         *
+         * @param node The node at which the states must be reconstructed.
+         * @param ancestors The array containing the ancestor matrix.
+         * @param lArray The likelihood array containing stored likelihood values.
+         * @param cArray The character state corresponding to likelihood values, read FastML paper for more info.
+         * @param siteNumber  Site number that all the computation is done on.
+         * @param newRootId The id of new root for the subtree we work on.
+         * @see [Pupko, et al., 2000, Molecular biology and evolution]
+         */
+        void getAncestralStatesForSite(const Node *node, std::map<int, std::vector<int>> &ancestors, VVdouble &lArray,
+                                       VVdouble &cArray, const size_t siteNumber, const int newRootId) const;
+
+
+        /**
+        * @brief Get all ancestral states for all nodes including gap (aka joint ancestral sequence reconstruction using DP approach with indel).
+        *
+         * @note This method considers gap as an additional character.
+        *
+        * Call the recursiveJointAncestralStates() method on each subtree per site.
+        *
+        * @return A map with nodes id as key, and a vector of states indices as value.
+        * @see JointAncestralSequenceReconstruction and [Pupko, et al., 2000, Molecular biology and evolution]
+        */
+        std::map<int, std::vector<int>> getAllAncestralStatesWGap() const;
+
+
+        /**
+        * @brief Get Joint ancestral sequences for all nodes including gap (aka joint ancestral sequence reconstruction using DP approach with indel).
+        *
+        * @note This method considers gap as an additional character.
+        *
+        * Call the getAncestralSequences() method.
+        * @return
+        */
+        AlignedSequenceContainer *JointAncestralSequencesReconstruction(){
+            return getAncestralSequences();
+        }
+
+
+
+        void recursiveComputeLikelihoodAtSite(const Node *node, const TreeTemplate<Node> *tree,
+                                              VVdouble &likelihoodArray, VVdouble &characterArray,
+                                              const size_t siteNumber, const int newRootId) const;
+
+        /****************************************** Based class functions **********************************************/
+
+        std::map<int, std::vector<size_t>> getAllAncestralStates() const override {};
+        // We can not use this definition cause "std::vector<size_t>": for gap state we have -1 which need int type.
+
+        /**
+        * @brief Get the ancestral sequence for a given node.
+        *
+        * This method is mainly for efficient internal use in other classes.
+        * Consider using the recursiveGetAncestralStatesForAllNodes() method for a more general output.
+        *
+        * @param nodeId The id of the node at which the sequence must be reconstructed.
+        * @return A sequence object.
+        */
+        Sequence *getAncestralSequenceForNode(int nodeId) const;
+
+        /**
+        * @brief Get ancestral states for a given node as a vector of int.
+        *
+        * The size of the vector depends on the implementation.
+        * This method is mainly for efficient internal use in other classes.
+        * Consider using the getAncestralSequenceForNode() method for a more
+        * general output.
+        *
+        * @param nodeId the id of the node at which the states must be reconstructed.
+        * @return A vector of states indices.
+        * @see getAncestralSequenceForNode
+        */
+        std::vector<size_t> getAncestralStatesForNode(int nodeId) const {};// todo: Marginal should be implemented and this bug should be fixed.
+        // We can not use this definition cause "std::vector<size_t>": for gap state we have -1 which need int type.
+
+
+
+        /**
+        * @brief Get Joint ancestral sequences for all nodes including gap (aka joint ancestral sequence reconstruction using DP approach with indel).
+        *
+        * @note This method considers gap as an additional character.
+        *
+        * Call the getAllAncestralStates() method and map the sites to the original one.
+        * @return A new SiteContainer object containing joint ancestral sequences.
+        */
+        AlignedSequenceContainer *getAncestralSequences() const ;
+
+
+    private:
+
+        /**
+         *  This method is called when the joint ancestral state reconstruction is called by getAllAncestralStates().
+         *
+         *  This method is designed for internal usage @see getAllAncestralStates().
+         *
+         * @param node  The node at which ancestral is reconstructed.
+         * @param tree  The subtree at which the computation is done.
+         * @param ancestors A vector of states indices.
+         * @param likelihoodArray  The likelihood matrix.
+         * @param cLikelihoodArray  The character states corresponding to likelihood array.
+         * @param data The sequences that the computation is done on.
+         * @param siteNumber Site number that all the computation is done on.
+         * @param newRootId The id of new root for the subtree we work on.
+         * @param isVisited A flag to indicate whether the node is visited or not.
+         */
+        void recursiveJointAncestralStates( const Node *node, const TreeTemplate<Node> *tree,
+                                            std::map<int, std::vector<int> > &ancestors,
+                                            VVVdouble &likelihoodArray, VVVdouble &cLikelihoodArray,
+                                            AlignedSequenceContainer &data, const size_t siteNumber,
+                                            const int newRootId, bool isVisited) const;
+
+
+    };
+} // end of namespace bpp.
+#endif //ARPIP_PIPANCESTRALSTATERECONSTRUCTION_H
